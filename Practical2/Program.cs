1a1e8612dbc9cb09d8c3be145884567ddb4e048a
hembhagat <hembhagat99@gmail.com>
2020-01-02 09:24:12 +0530

    User interaction practical for console C# application added

---
+++ Practical2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Practical2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, country;
+            while(true)
+            {
+                Console.Write("Please enter your name: ");
+                name = Console.ReadLine();
+                Console.WriteLine("Where are you from?");
+                country = Console.ReadLine();
+
+                if(Regex.IsMatch(name, @"^[a-zA-Z]+$") && Regex.IsMatch(country, @"^[a-zA-Z]+$"))
+                {
+                    Console.WriteLine("\nWelcome " + name + " from country " + country);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nPlease enter valid name or country!!");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
