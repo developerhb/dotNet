091e3336eca4410b3f9758e5068a53886786e71f
hembhagat <hembhagat99@gmail.com>
2020-01-16 08:38:39 +0530

    Practical on implementation inheritence

---
+++ Practical3/CarSalesman.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical3
+{
+    class CarSalesman : Salesman
+    {
+        public CarSalesman(String firstName, String lastName) : base(firstName,lastName)
+        {
+
+        }
+
+        public override void sell()
+        {
+            Console.WriteLine("Hello. My name is " + firstName + " " + lastName + ". I recommend you to by this car.");
+        }
+    }
+}
